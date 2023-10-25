a8f952d952928bc8642d395c960fa2a155d50a34
cpapanicholas <cpapanicholas@gmail.com>
2023-10-25 16:39:24 -0500

    ran into multiple issues but the main one will not let me get past the logo name

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,42 +1,47 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-const { createSVG } = require('./svgGenerator');
+const SVG = require('@svgdotjs/svg.js'); // Use the 'svg.js' library
+const { createSVG } = require('./logoGenerator');
 
 const main = async () => {
-    try {
-      const answers = await inquirer.prompt([
-        {
-          type: 'input',
-          name: 'text',
-          message: 'Enter the text for your logo:',
-        },
-        {
-          type: 'list',
-          name: 'shape',
-          message: 'Select a shape for your logo:',
-          choices: ['Circle', 'Square', 'Triangle'],
-        },
-        {
-          type: 'list',
-          name: 'color',
-          message: 'Select a color for your logo:',
-          choices: ['Red', 'Green', 'Blue', 'Yellow'],
-        },
-        {
-          type: 'input',
-          name: 'filename',
-          message: 'Enter the filename for the SVG:',
-          default: 'logo.svg',
-        },
-      ]);
+  try {
+    const answers = await inquirer.prompt([
+      {
+        type: 'input',
+        name: 'text',
+        message: 'Enter up to three characters for the text:',
+        validate: (input) => input.length <= 3,
+      },
+    ]);
 
-          // Create the SVG logo using the provided answers.
-    const svg = createSVG(answers.text, answers.shape, answers.color);
+    // Create the SVG element and set the dimensions.
+    const draw = SVG().size(300, 200);
+
+    // Create the shape based on the user's choice (e.g., 'circle', 'square', 'triangle').
+    let element;
+    if (answers.shape === 'circle') {
+      element = draw.circle(150); // Circle with a radius of 75 (diameter of 150).
+    } else if (answers.shape === 'square') {
+      element = draw.rect(150, 150); // Square with sides of 150.
+    } else if (answers.shape === 'triangle') {
+      element = draw.polygon('75,0 150,150 0,150'); // Triangle coordinates.
+    }
+
+    // Set the fill color for the shape.
+    element.fill(answers.shapeColor);
+
+    // Add text to the SVG.
+    draw.text(answers.text).move(100, 100); // Adjust text position.
+
+    // Serialize the SVG to a string.
+    const svgString = draw.svg();
+
+    const svg = createSVG(answers.text, answers.shape, answers.textColor, answers.shapeColor);
 
     // Save the SVG to a file.
-    fs.writeFileSync(answers.filename, svg);
+    fs.writeFileSync('logo.svg', svgString);
 
-    console.log(`Logo generated and saved as ${answers.filename}`);
+    console.log('Generated logo.svg');
   } catch (error) {
     console.error('An error occurred:', error);
   }
